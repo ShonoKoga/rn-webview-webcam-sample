ab8d07d9b80e72ecb2466f623360c312eaf86d02
ShonoKoga <shono.koga@gmail.com>
2020-11-12 17:09:35 +0900

    Set URL

--- native/App.tsx
+++ native/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {WebView} from 'react-native-webview';
 
+const uri = 'https://rn-webview-webcam-sample.vercel.app/';
+
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <WebView source={{uri: ''}} style={{flex: 1}} />
+      <WebView source={{uri}} style={{flex: 1}} />
     </SafeAreaView>
   );
 };
