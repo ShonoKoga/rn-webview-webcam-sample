287e60ac46946eeb9462e61f175ce6bcb474aea6
ShonoKoga <shono.koga@gmail.com>
2020-11-13 16:29:00 +0900

    Use react-native-inappbrowser-reborn

--- native/App.tsx
+++ native/App.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {WebView} from 'react-native-webview';
+import {View, Linking, Alert, Button} from 'react-native';
+import {InAppBrowser} from 'react-native-inappbrowser-reborn';
+// import {WebView} from 'react-native-webview';
 
-const uri = 'https://rn-webview-webcam-sample.vercel.app/';
+const url = 'https://rn-webview-webcam-sample.vercel.app/';
 
 const App = () => {
+  const openLink = async () => {
+    console.warn(InAppBrowser.isAvailable());
+    try {
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.open(url, {
+          // iOS Properties
+          dismissButtonStyle: 'cancel',
+          preferredBarTintColor: '#453AA4',
+          preferredControlTintColor: 'white',
+          readerMode: false,
+          animated: true,
+          modalPresentationStyle: 'fullScreen',
+          modalTransitionStyle: 'coverVertical',
+          modalEnabled: true,
+          enableBarCollapsing: false,
+          // Android Properties
+          showTitle: true,
+          toolbarColor: '#6200EE',
+          secondaryToolbarColor: 'black',
+          enableUrlBarHiding: true,
+          enableDefaultShare: true,
+          forceCloseOnRedirection: false,
+          // Specify full animation resource identifier(package:anim/name)
+          // or only resource name(in case of animation bundled with app).
+          animations: {
+            startEnter: 'slide_in_right',
+            startExit: 'slide_out_left',
+            endEnter: 'slide_in_left',
+            endExit: 'slide_out_right',
+          },
+          headers: {
+            'my-custom-header': 'my custom header value',
+          },
+        });
+        Alert.alert(JSON.stringify(result));
+      } else Linking.openURL(url);
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <WebView source={{uri}} style={{flex: 1}} />
-    </SafeAreaView>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={openLink} title="Try Camera" />
+    </View>
   );
 };
 
