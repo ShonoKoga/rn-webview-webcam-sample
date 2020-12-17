90ed1ebec1fc9fc0db94d095babfe3f815bee99f
shon0 <shono.koga@gmail.com>
2020-12-17 15:11:03 +0900

    Add getUserMedia

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,10 +1,25 @@
-import Webcam from "react-webcam";
+import React from 'react'
+// import Webcam from "react-webcam";
 
 export default () => {
+  React.useEffect(() => {
+    const getMedia = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          width: 250,
+          height: 250,
+        },
+        audio: false,
+      });
+      window.alert(stream)
+    };
+    getMedia();
+  }, []);
+
   return (
     <div>
       <h1>WebCam</h1>
-      <Webcam onUserMediaError={(e) => window.alert(e)}/>
+      {/* <Webcam onUserMediaError={(e) => window.alert(e)} /> */}
     </div>
   );
 };
